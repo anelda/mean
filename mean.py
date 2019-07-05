0ec4d4fad54ed6035af7c2aff0739cba8c568d2b
Anelda van der Walt <anelda.vdwalt@gmail.com>
2019-07-05 11:17:01 +0200

    Create mean and test_mean

---
+++ mean.py
@@ -0,0 +1,11 @@
+def mean(num_list):
+    if all(isinstance(x, complex) for x in num_list):
+        return NotImplemented
+    else:
+        try:
+            return sum(num_list)/len(num_list)
+        except ZeroDivisionError :
+            return 0
+        except TypeError as detail :
+            msg = "The algebraic mean of an non-numerical list is undefined.Please provide a list of numbers."
+            raise TypeError(detail.__str__() + "\n" +  msg)
